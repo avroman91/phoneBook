6e6ea7c70492a0b5c2c86cb973b005e6fd37124a
Roman Avdeienko <avroman91@gmail.com>
2021-08-09 13:21:12 +0300

    listUtils  - all List<T> changed to ContactList.

--- src/main/java/phoneBook/ua/ithillel/utils/ListUtils.java
+++ src/main/java/phoneBook/ua/ithillel/utils/ListUtils.java
@@ -14,69 +14,51 @@ import java.util.function.Predicate;
 
 
 public class ListUtils {
-    public static void main(String[] args) {
 
-        Integer[] arr = {1, 2, 3};
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        List<Integer> even = Arrays.asList(2,4,6,8,10,12);
-
-
-        forEach(arr, elem -> System.out.println(elem * elem));
-
-        System.out.println(anyMatch(list, i -> i % 2 == 0));
-        System.out.println(allMatch(even, i -> i % 2 == 0));
-
-        Integer sum = reduce(list, 1, Integer::sum);
-        System.out.println("Sum = " + sum);
-
-        List<String> reMapped = map(list, Object::toString);
-        System.out.println(reMapped);
-
-    }
-
-    public static <T> void forEach(T[] arr, Consumer<T> elemProcessor) {
-        for (T items : arr) {
-            elemProcessor.accept(items);
+    public static void forEach(ContactList contactList, Consumer<Contact> elemProcessor) {
+        for (int i = 0; i < contactList.size(); i++) {
+            elemProcessor.accept(contactList.get(i));
         }
     }
 
     public static Contact filter(ContactList contactList, Predicate<Contact> predicate) {
         for (int i = 0; i < contactList.size(); i++) {
-            if(predicate.test(contactList.get(i))){
+            if (predicate.test(contactList.get(i))) {
                 return contactList.get(i);
-            };
+            }
         }
         return null;
     }
 
-    public static <T, R> R reduce(List<T> list, R initValue, BiFunction<R, T, R> f) {
+    public static <R> R reduce(ContactList contactList, R initValue, BiFunction<R, Contact, R> biFunction) {
 
-        for (T elem : list) {
-            initValue = f.apply(initValue, elem);
+        for (int i = 0; i < contactList.size(); i++) {
+            initValue = biFunction.apply(initValue, contactList.get(i));
         }
         return initValue;
     }
 
-    public static <T, R> List<R> map(List<T> list, Function<T, R> function) {
-        List<R> result = new ArrayList<>();
-        for (T t : list) {
-            result.add(function.apply(t));
+    public static ContactList map(ContactList contactList, Function<Contact, Contact> function) {
+        ContactList result = new ContactList();
+        for (int i = 0; i < contactList.size(); i++) {
+            result.add(function.apply(contactList.get(i)));
         }
         return result;
     }
 
-    public static <T> boolean anyMatch(List<T> list, Predicate<T> predicate) {
-        for (T items : list) {
-            if (predicate.test(items)) {
+    public static boolean anyMatch(ContactList contactList, Predicate<Contact> predicate) {
+        for (int i = 0; i < contactList.size(); i++) {
+            if (predicate.test(contactList.get(i))) {
                 return true;
             }
         }
         return false;
     }
 
-    public static <T> boolean allMatch(List<T> list, Predicate<T> predicate) {
-        for (T items : list) {
-            if (!predicate.test(items)) {
+
+    public static boolean allMatch(ContactList contactList, Predicate<Contact> predicate) {
+        for (int i = 0; i < contactList.size(); i++) {
+            if (!predicate.test(contactList.get(i))) {
                 return false;
             }
         }
